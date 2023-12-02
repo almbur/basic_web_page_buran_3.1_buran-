fa2c0e3c1dfa3f1e96a8220df3cd20ca9b9ebf6e
almbur <albu2023@yahoo.com>
2023-12-02 17:17:18 -0500

    Your commit message here

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
+// Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/" style={{ backgroundColor: '#2196F3' }}>Home</Link></li>
-          <li><Link to="/products" style={{ backgroundColor: '#FF9800' }}>Products</Link></li>
-          <li><Link to="/about" style={{ backgroundColor: '#E91E63' }}>About Us</Link></li>
+    <header style={{ backgroundColor: '#333', padding: '10px 0' }}>
+      <nav style={{ textAlign: 'center' }}>
+        <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
+          <li style={{ display: 'inline-block', marginRight: '20px' }}>
+            <Link to="/" style={{ textDecoration: 'none', color: '#fff', padding: '10px 20px', borderRadius: '5px', transition: 'background-color 0.3s ease-in-out', backgroundColor: '#2196F3' }}>Home</Link>
+          </li>
+          <li style={{ display: 'inline-block', marginRight: '20px' }}>
+            <Link to="/products" style={{ textDecoration: 'none', color: '#fff', padding: '10px 20px', borderRadius: '5px', transition: 'background-color 0.3s ease-in-out', backgroundColor: '#FF9800' }}>Products</Link>
+          </li>
+          <li style={{ display: 'inline-block' }}>
+            <Link to="/about" style={{ textDecoration: 'none', color: '#fff', padding: '10px 20px', borderRadius: '5px', transition: 'background-color 0.3s ease-in-out', backgroundColor: '#E91E63' }}>About Us</Link>
+          </li>
         </ul>
       </nav>
     </header>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './ProductList.css'; // Import the CSS file
+import '../components/ProductList.css';
 
 const ProductList = () => {
   const products = [
